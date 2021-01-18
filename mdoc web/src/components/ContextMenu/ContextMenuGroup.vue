ff05f2f5597d7ab4ad5e62b175a03f91e1e529ae
mercurli <191104855@qq.com>
2021-01-19 00:34:24 +0800

    v 0.1.5

---
+++ mdoc web/src/components/ContextMenu/ContextMenuGroup.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="contextmenu-group">
+    <ul v-if="visible">
+      <slot />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ContextMenuGroup',
+  data () {
+    return {
+      visible: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    padding: 0;
+  }
+  .contextmenu-group {
+    border-bottom: 1px solid #f1f2f3;
+  }
+</style>
